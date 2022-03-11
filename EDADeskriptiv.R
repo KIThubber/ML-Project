059b5cf15eb4645bdd15cf61fb21361ad5e34792
Z00481XT <leon.debatin@siemens.com>
2022-03-11 18:05:29 +0100

    EDA

--- EDADeskriptiv.R
+++ EDADeskriptiv.R
@@ -49,16 +49,21 @@ p<-ggplot(port_EDA, aes(x=G_average, fill=school, color=school)) +
 p
 
 
-##########EDA Age
-table(port_EDA$age)
-port_EDA$G_average_mean_school[port$school=="GP"] <- mean(port_EDA$G_average[port$school=="GP"],)
-port_EDA$G_average_mean_school[port$school=="MS"] <- mean(port_EDA$G_average[port$school=="MS"],)
+###########EDA address
 
-p<-ggplot(port_EDA, aes(x=G_average, fill=age, color=age)) +
-  geom_histogram(position="identity", alpha=0.)
+table(port_EDA$address)
+port_EDA$G_average_mean_address[port$address=="U"] <- mean(port_EDA$G_average[port$address=="U"],)
+port_EDA$G_average_mean_address[port$address=="R"] <- mean(port_EDA$G_average[port$address=="R"],)
+
+
+p<-ggplot(port_EDA, aes(x=G_average, fill=address, color=address)) +
+  geom_histogram(aes(y=..density..),position="identity", alpha=0.4)+
+  geom_density(alpha=0.2)+
+  geom_vline(data=port_EDA, aes(xintercept=G_average_mean_address, color=address),linetype="dashed")+
+  scale_x_continuous(breaks=c(2,4,6,8,10,12,14,16,18,20))
 p
-p+geom_vline(data=port_EDA, aes(xintercept=G_average_mean_school, color=school),
-             linetype="dashed")
+
+
 
 
 
@@ -66,5 +71,29 @@ p+geom_vline(data=port_EDA, aes(xintercept=G_average_mean_school, color=school),
 
 table(port_EDA$famsize)
 
+
+
+
+
+
 ######EDA Mjob
 table(port_EDA$Mjob)
+
+port_EDA$G_average_mean_Mjob[port$Mjob=="at_home"] <- mean(port_EDA$G_average[port$Mjob=="at_home"],)
+port_EDA$G_average_mean_Mjob[port$Mjob=="health"] <- mean(port_EDA$G_average[port$Mjob=="health"],)
+port_EDA$G_average_mean_Mjob[port$Mjob=="other"] <- mean(port_EDA$G_average[port$Mjob=="other"],)
+port_EDA$G_average_mean_Mjob[port$Mjob=="services"] <- mean(port_EDA$G_average[port$Mjob=="services"],)
+port_EDA$G_average_mean_Mjob[port$Mjob=="teacher"] <- mean(port_EDA$G_average[port$Mjob=="teacher"],)
+
+p<-ggplot(port_EDA, aes(x=G_average, fill=Mjob, color=Mjob)) +
+  geom_density(position="identity", alpha=0.2)+
+  geom_vline(data=port_EDA, aes(xintercept=G_average_mean_Mjob, color=Mjob),linetype="dashed")+
+  scale_x_continuous(breaks=c(2,4,6,8,10,12,14,16,18,20))
+p
+
+
+sum(table(port_EDA$Mjob))
+
+
+##### allle kategorialen variablen: school, sex, address, famsize, pstatus, medu, fedu, mjob, fjob, reson, guardian, schoolsup, famsup, paid, activities, nursey, higher, internet, romantic
+
